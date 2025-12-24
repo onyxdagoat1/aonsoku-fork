788c34aa64ab32dc1ada0e9a353fcc17f57d5733
onyxdagoat1 <aecloud2004@gmail.com>
2025-12-24 23:28:32 +0100

    Restore original routesList.ts structure and add REGISTER route

--- src/routes/routesList.ts
+++ src/routes/routesList.ts
@@ -1,23 +1,78 @@
-export const routeList = {
-  ROOT: '/',
-  LOGIN: '/login',
-  REGISTER: '/register',
-  LIBRARY: '/library',
-  ALBUMS: '/library/albums',
-  ALBUM: '/library/albums/:id',
-  ARTISTS: '/library/artists',
-  ARTIST: '/library/artists/:id',
-  SONGS: '/library/songs',
-  PLAYLISTS: '/library/playlists',
-  PLAYLIST: '/library/playlists/:id',
-  GENRES: '/library/genres',
-  GENRE: '/library/genres/:id',
-  RADIOS: '/library/radios',
-  RADIO: '/library/radios/:id',
-  SEARCH: '/search',
-  SETTINGS: '/settings',
-  FAVORITES: '/favorites',
-  NOWPLAYING: '/nowplaying',
-} as const;
+import { AlbumListType } from '@/types/responses/album'
+import { AlbumsFilters, YearFilter } from '@/utils/albumsFilter'
 
-export type RoutePath = (typeof routeList)[keyof typeof routeList];
+const LIBRARY = {
+  HOME: '/',
+  ARTISTS: '/library/artists',
+  SONGS: '/library/songs',
+  ALBUMS: '/library/albums',
+  PLAYLISTS: '/library/playlists',
+  PODCASTS: '/library/podcasts',
+  EPISODES: '/library/episodes',
+  RADIOS: '/library/radios',
+}
+
+const ARTIST = {
+  PAGE: (artistId: string) => `${LIBRARY.ARTISTS}/${artistId}`,
+  PATH: `${LIBRARY.ARTISTS}/:artistId`,
+}
+
+const ALBUM = {
+  PAGE: (albumId: string) => `${LIBRARY.ALBUMS}/${albumId}`,
+  PATH: `${LIBRARY.ALBUMS}/:albumId`,
+}
+
+const ALBUMS = {
+  GENRE: (genre: string) =>
+    `${LIBRARY.ALBUMS}?filter=${AlbumsFilters.ByGenre}&genre=${encodeURIComponent(genre)}`,
+  ARTIST: (id: string, name: string) =>
+    `${LIBRARY.ALBUMS}?filter=${AlbumsFilters.ByDiscography}&artistId=${id}&artistName=${encodeURIComponent(name)}`,
+  RECENTLY_PLAYED: `${LIBRARY.ALBUMS}?filter=${AlbumsFilters.RecentlyPlayed}`,
+  MOST_PLAYED: `${LIBRARY.ALBUMS}?filter=${AlbumsFilters.MostPlayed}`,
+  RECENTLY_ADDED: `${LIBRARY.ALBUMS}?filter=${AlbumsFilters.RecentlyAdded}`,
+  RANDOM: `${LIBRARY.ALBUMS}?filter=${AlbumsFilters.Random}`,
+  SEARCH: (query: string) =>
+    `${LIBRARY.ALBUMS}?filter=${AlbumsFilters.Search}&query=${encodeURIComponent(query)}`,
+  YEAR: (yearFilter: YearFilter) =>
+    `${LIBRARY.ALBUMS}?filter=${AlbumsFilters.ByYear}&yearFilter=${yearFilter}`,
+  GENERIC: (filter: AlbumListType) => `${LIBRARY.ALBUMS}?filter=${filter}`,
+}
+
+const SONGS = {
+  SEARCH: (query: string) =>
+    `${LIBRARY.SONGS}?filter=${AlbumsFilters.Search}&query=${encodeURIComponent(query)}`,
+  ARTIST_TRACKS: (id: string, name: string) =>
+    `${LIBRARY.SONGS}?artistId=${id}&artistName=${encodeURIComponent(name)}`,
+}
+
+const PLAYLIST = {
+  PAGE: (playlistId: string) => `${LIBRARY.PLAYLISTS}/${playlistId}`,
+  PATH: `${LIBRARY.PLAYLISTS}/:playlistId`,
+}
+
+const PODCASTS = {
+  PAGE: (podcastId: string) => `${LIBRARY.PODCASTS}/${podcastId}`,
+  PATH: `${LIBRARY.PODCASTS}/:podcastId`,
+}
+
+const EPISODES = {
+  PAGE: (episodeId: string) => `${LIBRARY.EPISODES}/${episodeId}`,
+  PATH: `${LIBRARY.EPISODES}/:episodeId`,
+  LATEST: `${LIBRARY.EPISODES}/latest`,
+}
+
+const SERVER_CONFIG = '/server-config'
+const REGISTER = '/register'
+
+export const ROUTES = {
+  LIBRARY,
+  ARTIST,
+  ALBUM,
+  ALBUMS,
+  SONGS,
+  PLAYLIST,
+  PODCASTS,
+  EPISODES,
+  SERVER_CONFIG,
+  REGISTER,
+}
