dc5f988ec68a8e39451274b66721c49194e174be
onyxdagoat1 <aecloud2004@gmail.com>
2025-12-24 04:41:40 +0100

    refactor: remove audio and content page references from settings

--- src/app/components/settings/pages/index.tsx
+++ src/app/components/settings/pages/index.tsx
@@ -2,14 +2,10 @@ import { SettingsOptions } from '@/app/components/settings/options'
 import { useAppSettings } from '@/store/app.store'
 import { Accounts } from './accounts'
 import { Appearance } from './appearance'
-import { Audio } from './audio'
-import { Content } from './content'
 import { Desktop } from './desktop'
 
 const pages: Record<SettingsOptions, JSX.Element> = {
   appearance: <Appearance />,
-  audio: <Audio />,
-  content: <Content />,
   accounts: <Accounts />,
   desktop: <Desktop />,
 }
